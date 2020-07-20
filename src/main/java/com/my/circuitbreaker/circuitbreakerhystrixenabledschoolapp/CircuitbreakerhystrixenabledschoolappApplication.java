440af926d42475974af2d9654abe159ceedec976
root <root@localhost.localdomain>
2020-07-20 16:47:52 +0530

    initial commit with code skeleton

---
+++ src/main/java/com/my/circuitbreaker/circuitbreakerhystrixenabledschoolapp/CircuitbreakerhystrixenabledschoolappApplication.java
@@ -0,0 +1,13 @@
+package com.my.circuitbreaker.circuitbreakerhystrixenabledschoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerhystrixenabledschoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerhystrixenabledschoolappApplication.class, args);
+	}
+
+}
